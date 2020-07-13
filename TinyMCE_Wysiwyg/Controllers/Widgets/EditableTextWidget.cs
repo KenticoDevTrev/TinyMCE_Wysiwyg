a70ced844ddc334cdd62fa0fe086c7a034fc9f95
Trevor Fayas <tfayas@hbs.net>
2020-07-13 13:51:15 -0500

    Removed sample widget that was there by mistake, added more descriptions and altered title to hopefully reduce confusion with Froala editor

--- TinyMCE_Wysiwyg/Controllers/Widgets/EditableTextWidget.cs
+++ TinyMCE_Wysiwyg/Controllers/Widgets/EditableTextWidget.cs
@@ -1,4 +1,4 @@
 ﻿using HBS.TinyMCE_Wysiwyg;
 using Kentico.PageBuilder.Web.Mvc;
 
-[assembly: RegisterWidget("HBS.EditableText", "Editable Text", propertiesType : typeof(EditableTextWidgetProperties), customViewName: "Widgets/_EditableTextWidget", Description = "Provides an editable text region that can be used to enter page content.", IconClass = "icon-w-editable-text"  )]
+[assembly: RegisterWidget("HBS.EditableText", "Editable Text (TinyMCE)", propertiesType : typeof(EditableTextWidgetProperties), customViewName: "Widgets/_EditableTextWidget", Description = "Provides an editable text region that can be used to enter page content. Not needed if you use Froala.", IconClass = "icon-w-editable-text"  )]
